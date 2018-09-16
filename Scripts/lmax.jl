bdb3296cd06cba885eecb466fdcf8618307032c7
Leandro Martinez <leandro@iqm.unicamp.br>
2018-09-16 16:36:29 -0300

    added user_max_d option

--- Scripts/lmax.jl
+++ Scripts/lmax.jl
@@ -1,7 +1,7 @@
 #
 # Script to plot Lmax as a function of the percentage cutoff
 #
-# Run with: julia lmax.jl file1.dat lmax.pdf
+# Run with: julia lmax.jl file1.dat lmax.pdf [user_max_d]
 #
 # file1.dat:
 # File containing the CB-CB distances for all Nz-Nz distances
@@ -11,10 +11,9 @@
 # lmax.pdf
 # the name of the plot that will generated
 #
-# 102mA00.log: 16 56 21.316 23.336
-#
-# where the two last numbers (columns 4 and 5) are read and must
-# be the eucliean and topological distance of the pair.
+# user_max_d is an optional real parameter indicating the maximum
+# tolerated distance, assuming that something greater than that is 
+# probably an error
 #
 # In the examples, the files have names as 
 # file1 = ./data/KK_I_CB_t
@@ -35,12 +34,43 @@ let LmaxTest
   file1 = ARGS[1]
   plot1 = ARGS[2]
   
-  ndata, dnz, dcb = read_data(file1,cols=[1,2])
+  ndata_read, dnz_read, dcb_read = read_data(file1,cols=[1,2])
 
   Lmax = [ 0. for i in 1:100 ]
   p_Lmax = [ Float64(i)/100. for i in 1:100 ]
           
+  # Removing outliers (data that does not fit user_max_d)
+
+  if length(ARGS) == 3
+    user_max_d = parse(Float64,ARGS[3])
+    nremove = 0
+    for i in 1:ndata_read
+      if dcb_read[i] > user_max_d
+        nremove = nremove + 1
+      end
+    end
+    remfrac = @sprintf "%.2f" 100*float(nremove)/ndata_read
+    println(" Will remove ", nremove, 
+            " (", remfrac, "%) points with d > user_max_d ")
+    ndata = ndata_read - nremove
+    dnz = Vector{Float64}(undef,ndata)
+    dcb = Vector{Float64}(undef,ndata)
+    j = 0
+    for i in 1:ndata_read
+      if dcb_read[i] <= user_max_d
+        j = j + 1
+        dnz[j] = dnz_read[i]
+        dcb[j] = dcb_read[i]
+      end
+    end
+  else
+    ndata = ndata_read
+    dnz = dnz_read
+    dcb = dcb_read
+  end
   Lmax[100] = maximum(dcb)
+
+  println(" Lmax[100] = ", Lmax[100])
   step = 0.1
 
   nd = Int64( trunc((1. / step)*(Lmax[100]+1.)) )
@@ -78,6 +108,12 @@ let LmaxTest
     y = [Lmax[i],Lmax[i]]; plot(x,y,"--",color="black",linewidth=0.5)
     text(0.,Lmax[i]+0.1, @sprintf "f = %.2f / Lmax = %.1f" i/100. Lmax[i])
   end
+  # Theoretical (user-provided) maximum length
+  if length(ARGS) == 3
+    y = [user_max_d,user_max_d]; plot(x,y,"--",color="black",linewidth=0.5)
+    text(0.,user_max_d+0.1, @sprintf "Extended linker, Lmax = %.1f" user_max_d)
+  end 
+
   xlabel("Cutoff Fraction")
   ylabel("Lmax(Cutoff Fraction)")
   savefig(plot1)
